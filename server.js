a642ec3b649fd49c8ae14f045408e46316f9a7e1
Yist Lin <yishen992@gmail.com>
2016-06-17 20:12:02 +0800

    modify something to suit the standard of OpenShift

--- server.js
+++ server.js
@@ -3,13 +3,14 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.get('/', function(request, response) {
     response.send('Hello world!');
 });
 
-var server = app.listen(80, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('Server listening at http://%s:%s', host, port);
+var server = app.listen(server_port, server_ip_address, function() {
+    console.log('Server listening at http://%s:%s', server_ip_address, server_port);
 });
 
