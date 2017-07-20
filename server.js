b277dad4548c3e342f7f97d809fecc83d91e0751
Yist Lin <yishen992@gmail.com>
2017-07-20 15:48:46 +0800

    set up ip and port by express config

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 app.use(parser.urlencoded({extended: true}));
 app.use(parser.json());
@@ -13,13 +13,14 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 // get JSON file of bibles
-var bible_en = JSON.parse(fs.readFileSync('data/bible_english_new.json', 'utf8'));
+// var bible_en = JSON.parse(fs.readFileSync('data/bible_english_new.json', 'utf8'));
 
 app.get('/', (request, response) => {
     // render views/index.ejs
-    response.render('index.ejs',
-        {text: bible_en[0]['chapters'][0]['verses']}
-        );
+    // response.render('index.ejs',
+    //     {text: bible_en[0]['chapters'][0]['verses']}
+    //     );
+    response.render('index.ejs');
 });
 
 app.get('/chapter', (request, response) => {
@@ -40,6 +41,6 @@ app.post('/quotes', (request, response) => {
 app.put('/quotes', (request, response) => {
 });
 
-app.listen(server_port, server_ip_address, function() {
-	console.log('Listening on ' + server_ip_address + ', server_port ' + server_port);
+app.listen(app.get('port'), app.get('ip'), function() {
+	console.log('Express(Node.js) server is listening at %s:%d', app.get('ip'), app.get('port'));
 });
